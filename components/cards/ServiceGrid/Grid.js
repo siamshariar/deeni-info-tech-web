6852a8b87a1778bf2289bf8329ed93ca90e009d2
nazmul <typetonazmul@gmail.com>
2022-02-01 09:52:50 +0600

    Fix grid height width

--- components/cards/ServiceGrid/Grid.js
+++ components/cards/ServiceGrid/Grid.js
@@ -2,40 +2,64 @@ const ServiceGrid = () => {
   return (
     <div className="grid grid-cols-4 gap-2">
       <div className="col-span-2 row-span-1 text-center text-white service-grid">
-        <span>01</span>
+        <div className="service-grid-content">
+          <span>01</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid">
-        <span>02</span>
+        <div className="service-grid-content">
+          <span>02</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid">
-        <span>03</span>
+        <div className="service-grid-content">
+          <span>03</span>
+        </div>
       </div>
       <div className="col-span-2 row-span-1 text-center text-white service-grid">
-        <span>04</span>
+        <div className="service-grid-content">
+          <span>04</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid">
-        <span>05</span>
+        <div className="service-grid-content">
+          <span>05</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid">
-        <span>06</span>
+        <div className="service-grid-content">
+          <span>06</span>
+        </div>
       </div>
       <div className="col-span-2 row-span-1 text-center text-white service-grid">
-        <span>07</span>
+        <div className="service-grid-content">
+          <span>07</span>
+        </div>
       </div>
       <div className="col-span-2 row-span-1 text-center text-white service-grid">
-        <span>08</span>
+        <div className="service-grid-content">
+          <span>08</span>
+        </div>
       </div>
       <div className="col-span-2 row-span-1 text-center text-white service-grid">
-        <span>09</span>
+        <div className="service-grid-content">
+          <span>09</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid">
-        <span>10</span>
+        <div className="service-grid-content">
+          <span>10</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid">
-        <span>11</span>
+        <div className="service-grid-content">
+          <span>11</span>
+        </div>
       </div>
       <div className="col-span-2 row-span-1 text-center text-white service-grid">
-        <span>12</span>
+        <div className="service-grid-content">
+          <span>12</span>
+        </div>
       </div>
     </div>
   );
--- components/cards/ServiceGrid/Grid2.js
+++ components/cards/ServiceGrid/Grid2.js
@@ -4,76 +4,101 @@ const ServiceGrid = () => {
   return (
     <div className="grid grid-cols-4 gap-2">
       <div className="col-span-2 row-span-1 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>01</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>01</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>02</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>02</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>03</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>03</span>
+        </div>
       </div>
       <div className="col-span-2 row-span-1 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>04</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>04</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>05</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>05</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>06</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>06</span>
+        </div>
       </div>
       <div className="col-span-2 row-span-1 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>07</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>07</span>
+        </div>
       </div>
       <div className="col-span-2 row-span-1 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>08</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>08</span>
+        </div>
       </div>
       <div className="col-span-2 row-span-1 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>09</span>
+        <div className="service-grid-content">
+          {" "}
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>09</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>10</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>10</span>
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>11</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>11</span>
+        </div>
       </div>
       <div className="col-span-2 row-span-1 text-center text-white service-grid service-grid2">
-        <span className="grid-icon">
-          <ReactIcon />
-        </span>
-        <span>12</span>
+        <div className="service-grid-content">
+          <span className="grid-icon">
+            <ReactIcon />
+          </span>
+          <span>12</span>
+        </div>
       </div>
     </div>
   );

0df4e777272cfc0d0b9232bf90c199a57c966f61
nazmul <typetonazmul@gmail.com>
2022-12-25 19:21:17 +0600

    Add client end validation to subscription form

--- src/components/ui/Subscription/index.jsx
+++ src/components/ui/Subscription/index.jsx
@@ -15,14 +15,33 @@ const Subscription = () => {
     setValue(newValue);
   };
 
-  const handleValueChange = (e, el, key) => {
+  const handleValueChange = (e, key) => {
     // el.current.classList.remove(styles.error);
+    e.target.classList.remove(styles.error);
     changeValue(key, e.target.value.trim());
     // console.log(value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    let error = false;
+
+    const pattern =
+      /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+
+    if (value.email === '') {
+      emailEl.current.classList.add(styles.error);
+      error = true;
+    }
+
+    if (!pattern.test(value.email)) {
+      emailEl.current.classList.add(styles.error);
+      error = true;
+    }
+
+    if (error) return;
 
     let data = {
       email: value.email,
@@ -75,8 +94,9 @@ const Subscription = () => {
                 type="text"
                 name="email"
                 placeholder="Enter your email"
-                onChange={(e) => handleValueChange(e, emailEl, 'email')}
+                onChange={(e) => handleValueChange(e, 'email')}
                 value={value.email}
+                ref={emailEl}
             />
             <button type="submit" name="submit">
               Subscribe

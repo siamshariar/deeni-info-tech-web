64e9f66d7f3fb276cbedc7b2b5eb610436327687
Siam Al Sahariar <siam.it.int@gmail.com>
2025-05-10 14:43:56 +0600

    Add mailerlite api

--- src/pages/api/emailSub.js
+++ src/pages/api/emailSub.js
@@ -4,14 +4,28 @@ export default async function (req, res) {
   console.log("email")
   console.log(email)
 
-  const result = await fetch('https://ztechltd.com/w/dit-email-sub.php?email='+email, {
-    method: 'GET',
+  const MAILERLITE_API_KEY = process.env.MAILERLITE_API_KEY;
+
+  const result = await fetch('https://api.mailerlite.com/api/v2/subscribers', {
+    method: 'POST',
     headers: {
+
+      Authorization: `Bearer ${MAILERLITE_API_KEY}`,
       Accept: 'application/json, text/plain, */*',
       'Content-Type': 'application/json',
     },
+    body: JSON.stringify({
+      email: email,
+      resubscribe: true, // Allows resubscription if email exists
+    }),
   });
 
 
-  res.status(200).json({ status: 'OK' });
+  if (result.status === 200 || result.status === 201) {
+    res.status(200).json({ status: 'OK' });
+  } else {
+    const errorData = await result.json();
+    console.error(errorData);
+    res.status(result.status).json({ status: 'FAILED', error: errorData });
+  }
 }
